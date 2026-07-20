b920146286d3c23faa7e7858f3e5f0f6be72a503
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-07-20 18:00:30 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="ticks">
+        <button class="btn btn-secondary">Button</button>
+        <button class="btn w-64 rounded-full">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
