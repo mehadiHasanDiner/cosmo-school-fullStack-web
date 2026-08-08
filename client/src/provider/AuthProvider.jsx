9d2d16c62dd8be6612076ec9b6cf115102d53cc8
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-08-09 02:39:03 +0600

    some component done

--- client/src/provider/AuthProvider.jsx
+++ client/src/provider/AuthProvider.jsx
@@ -1,12 +1,38 @@
 import { AuthContext } from "../context/AuthContext";
-import { getAuth } from "firebase/auth";
-import { app } from "../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
 
-const auth = getAuth(app);
+import { auth } from "../firebase/firebase.config";
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const registerUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // observe user Data
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      console.log(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const authInfo = {
-    user: "Mehadi",
+    user,
+    loading,
+    registerUser,
   };
 
   return <AuthContext value={authInfo}>{children}</AuthContext>;
