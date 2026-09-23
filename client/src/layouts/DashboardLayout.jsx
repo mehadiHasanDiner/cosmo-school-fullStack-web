77884bb3cecf2b48f574270ea94f3775e32b3a84
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-09-23 14:32:59 +0600

    all profile logic done

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,11 +1,24 @@
-import { Link, NavLink, Outlet } from "react-router";
-import { FaRegCreditCard } from "react-icons/fa";
-import { CiDeliveryTruck } from "react-icons/ci";
-import useAuth from "../hooks/useAuth";
-import logoImg from "./../assets/logo.png";
+import { Outlet } from "react-router";
+
+import useDbUser from "../hooks/useDbUser";
+import LoadingSpinner from "../components/common/LoadingSpinner";
+import DashboardContent from "./DashboardContent";
+import { useState } from "react";
 
 const DashboardLayout = () => {
-  const { user } = useAuth();
+  const { dbUser, isDbUserLoading } = useDbUser();
+  const availableRoles = dbUser?.roles || [];
+
+  // প্রথম available role default হিসেবে নিচ্ছি
+  const [activeRole, setActiveRole] = useState(availableRoles[0]);
+
+  if (isDbUserLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (!dbUser) {
+    return <div>User not found</div>;
+  }
 
   return (
     <div className="drawer lg:drawer-open">
@@ -35,11 +48,13 @@ const DashboardLayout = () => {
             </svg>
           </label>
           <div className="px-4">Cosmo Dashboard</div>
-          <p className="font-bold">Email: {user.role}</p>
+          <p className="font-bold">Email: {dbUser?.email}</p>
         </nav>
         {/* Page content here */}
         <div className="p-4">
-          <Outlet></Outlet>
+          <Outlet
+            context={{ availableRoles, activeRole, setActiveRole }}
+          ></Outlet>
         </div>
       </div>
 
@@ -52,83 +67,7 @@ const DashboardLayout = () => {
         <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
           {/* Sidebar content here */}
           <ul className="menu w-full grow">
-            {/* List item */}
-            <li>
-              <Link to="/">
-                <img src={logoImg} alt="" />
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/dashboard"
-                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                data-tip="Homepage"
-              >
-                {/* Home icon */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2"
-                  fill="none"
-                  stroke="currentColor"
-                  className="my-1.5 inline-block size-4"
-                >
-                  <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
-                  <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                </svg>
-                <span className="is-drawer-close:hidden">Homepage</span>
-              </Link>
-            </li>
-
-            {/* our dashboard links */}
-            <li>
-              <NavLink
-                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                data-tip="My Parcels"
-                to="/dashboard/my-parcels"
-              >
-                <CiDeliveryTruck size={18} />
-                <span className="is-drawer-close:hidden">My Parcels</span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                data-tip="Payment History"
-                to="/dashboard/payment-history"
-              >
-                <FaRegCreditCard size={18} />
-                <span className="is-drawer-close:hidden">Payment History</span>
-              </NavLink>
-            </li>
-
-            {/* List item */}
-            <li>
-              <button
-                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                data-tip="Settings"
-              >
-                {/* Settings icon */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2"
-                  fill="none"
-                  stroke="currentColor"
-                  className="my-1.5 inline-block size-4"
-                >
-                  <path d="M20 7h-9"></path>
-                  <path d="M14 17H5"></path>
-                  <circle cx="17" cy="17" r="3"></circle>
-                  <circle cx="7" cy="7" r="3"></circle>
-                </svg>
-                <span className="is-drawer-close:hidden">Settings</span>
-              </button>
-            </li>
+            <DashboardContent dbUser={dbUser} />
           </ul>
         </div>
       </div>
